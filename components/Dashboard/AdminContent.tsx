44ff76281a7a51e40f226a7c2b1722ded0b2346d
HackerShohag <HackerShohag@outlook.com>
2024-03-10 15:02:56 +0600

    fix

--- components/Dashboard/AdminContent.tsx
+++ components/Dashboard/AdminContent.tsx
@@ -130,7 +130,7 @@ export default function Content() {
             <>
               <ModalHeader className="flex flex-col gap-1">Bus Tickets</ModalHeader>
               <ModalBody>
-                <SeatLayout ticketStatus={{ available: "Available", booked: "Booked", occupied: "Reserved" }} setSeatsButton={function (seats: string[]): void {
+                <SeatLayout seatStatus={{ available: "Available", booked: "Booked", occupied: "Reserved" }} setSeatsButton={function (seats: string[]): void {
                   throw new Error("Function not implemented.");
                 }} />
                 <div className="flex felx-row justify-center gap-2">
